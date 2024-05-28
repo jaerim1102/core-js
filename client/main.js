a78b94050a9bc1d729392130c6ae913f26faba1d
jaerim1102 <jaerim1102@naver.com>
2024-05-28 14:52:48 +0900

    first commit

---
+++ client/main.js
@@ -0,0 +1,6 @@
+
+
+console.log('안녕 웹브라우저!');
+
+const a = 'hello';
+const b = '10';
---
+++ eslint.config.mjs
@@ -0,0 +1,20 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+
+
+export default [
+  pluginJs.configs.recommended,
+
+  {
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node
+      }
+    },
+    rules:{
+      "no-unused-vars": 'warn'
+    }
+  },
+
+];
